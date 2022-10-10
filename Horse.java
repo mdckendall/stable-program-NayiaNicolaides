f2ff0149182ca6750106680c76a0c9a8ddcd5fa6
Nayia Nicolaides <112590488+NayiaNicolaides@users.noreply.github.com>
2022-10-10 03:58:45 +0000

    output

--- Horse.java
+++ Horse.java
@@ -1,1 +1,17 @@
 //Create the Horse class here.
+public class Horse{
+  String hn;
+  Double w;
+  Boolean t;
+
+  public String toString() {
+    return "Horse #1 Name: "+hn+" Weight: "+w+" Tame: "+t+"";
+  }
+  
+  public Horse(String hn, double w, boolean t){
+    this.hn = hn;
+    this.w = w;
+    this.t = t;
+  }
+}
+
