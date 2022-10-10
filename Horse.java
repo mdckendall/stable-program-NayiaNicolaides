dfd93b094a55bb69dd048ef5b40d09c9e6fc5b33
Nayia Nicolaides <112590488+NayiaNicolaides@users.noreply.github.com>
2022-10-10 04:29:24 +0000

    fixed the code

--- Horse.java
+++ Horse.java
@@ -1,17 +1,21 @@
 //Create the Horse class here.
 public class Horse{
+  private static int nextCounter = 1;
   String hn;
   Double w;
   Boolean t;
+  private int counter = 0;
 
   public String toString() {
-    return "Horse #1 Name: "+hn+" Weight: "+w+" Tame: "+t+"";
+    return "Horse #"+counter+"  Name: "+hn+" Weight: "+w+" Tame: "+t+"";
   }
   
   public Horse(String hn, double w, boolean t){
     this.hn = hn;
     this.w = w;
     this.t = t;
+    counter = nextCounter;
+    nextCounter++;
   }
 }
 
